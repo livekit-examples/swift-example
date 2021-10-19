1966e04b9303ae04ed194e34fe898f062f37f9a4
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2021-10-19 12:50:32 +0900

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Hiroshi Horie on 2021/10/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/Multiplatform_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multiplatform_SwiftUIApp.swift
+//  Shared
+//
+//  Created by Hiroshi Horie on 2021/10/19.
+//
+
+import SwiftUI
+
+@main
+struct Multiplatform_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
