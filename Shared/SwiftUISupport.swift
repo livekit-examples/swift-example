7e8bd81b12c0a91967d880a23eb9f57b7edacd75
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2021-10-29 03:18:20 +0900

    ios/macOS `SwiftUIVideoView`

---
+++ Shared/SwiftUISupport.swift
@@ -0,0 +1,53 @@
+import SwiftUI
+import LiveKit
+
+#if !os(macOS)
+import UIKit
+public typealias NativeViewRepresentable = UIViewRepresentable
+#else
+// macOS
+import AppKit
+public typealias NativeViewRepresentable = NSViewRepresentable
+#endif
+
+/// A ``VideoView`` that can be used in SwiftUI.
+/// Supports both iOS and macOS.
+struct SwiftUIVideoView: NativeViewRepresentable {
+    /// Pass a ``VideoTrack`` of a ``Participant``.
+    let track: VideoTrack
+
+#if !os(macOS)
+    // iOS
+
+    func makeUIView(context: Context) -> VideoView {
+        let view = VideoView()
+        view.track = track
+        return view
+    }
+
+    func updateUIView(_ videoView: VideoView, context: Context) {
+        videoView.track = track
+    }
+
+    static func dismantleUIView(_ videoView: VideoView, coordinator: ()) {
+        videoView.track = nil
+    }
+#else
+    // macOS
+
+    func makeNSView(context: Context) -> VideoView {
+        let view = VideoView()
+        view.track = track
+        return view
+    }
+
+    func updateNSView(_ videoView: VideoView, context: Context) {
+        videoView.track = track
+    }
+
+    static func dismantleNSView(_ videoView: VideoView, coordinator: ()) {
+        videoView.track = nil
+    }
+
+#endif
+}
