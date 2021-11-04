d59b24133067d6997d000cae9ec8b7767d9336f6
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2021-11-05 00:24:48 +0900

    receive video dimensions with `SwiftUIVideoView`

--- Shared/SwiftUISupport.swift
+++ Shared/SwiftUISupport.swift
@@ -10,12 +10,54 @@ import AppKit
 public typealias NativeViewRepresentable = NSViewRepresentable
 #endif
 
+/// This class receives delegate events since a struct can't be used for a delegate
+class SwiftUIVideoViewDelegateReceiver: TrackDelegate {
+    
+    @Binding var dimensions: Dimensions?
+    
+    init(dimensions: Binding<Dimensions?> = .constant(nil)) {
+        self._dimensions = dimensions
+    }
+    
+    deinit {
+        print("SwiftUIVideoViewDelegateReceiver deinit")
+    }
+    
+    func track(_ track: VideoTrack,
+               videoView: VideoView,
+               didUpdate dimensions: Dimensions) {
+        self.dimensions = dimensions
+        print("SwiftUIVideoView got dimensions \(dimensions)")
+    }
+    
+    func track(_ track: VideoTrack, videoView: VideoView, didUpdate size: CGSize) {
+        print("SwiftUIVideoView got size \(size)")
+    }
+}
+
 /// A ``VideoView`` that can be used in SwiftUI.
 /// Supports both iOS and macOS.
 struct SwiftUIVideoView: NativeViewRepresentable {
     /// Pass a ``VideoTrack`` of a ``Participant``.
     let track: VideoTrack
-    var mode: VideoView.Mode = .fill
+    let mode: VideoView.Mode
+    @Binding var dimensions: Dimensions?
+    
+    let delegateReceiver: SwiftUIVideoViewDelegateReceiver
+
+    init(_ track: VideoTrack,
+         mode: VideoView.Mode = .fill,
+         dimensions: Binding<Dimensions?> = .constant(nil)) {
+        
+        self.track = track
+        self.mode = mode
+        self._dimensions = dimensions
+        
+        print("SwiftUIVideoView adding delegate")
+        self.delegateReceiver = SwiftUIVideoViewDelegateReceiver(dimensions: dimensions)
+        self.track.add(delegate: delegateReceiver)
+    }
+    
     #if !os(macOS)
     // iOS
 
