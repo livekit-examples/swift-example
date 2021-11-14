73cdebbd8331e1ab5a05be911d56115788256d11
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2021-11-14 07:39:57 +0700

    ipc screen share example

--- BroadcastExt/SampleHandler.swift
+++ BroadcastExt/SampleHandler.swift
@@ -7,42 +7,46 @@ import os.log
 let logger = OSLog(subsystem: "io.livekit.example.screen-broadcaster",
                    category: "Broadcaster")
 
-extension SampleHandler: RoomDelegate {
-    //
-}
-
 class SampleHandler: RPBroadcastSampleHandler {
 
-    var room: Room?
-    var videoTrack: LocalVideoTrack?
+    static let ipcName = "group.livekit-example.broadcast.buffer-ipc"
+    let ipcClient = IPCClient()
+
+    //    var room: Room?
+    //    var videoTrack: LocalVideoTrack?
 
     override func broadcastStarted(withSetupInfo setupInfo: [String: NSObject]?) {
-        // User has requested to start the broadcast. Setup info from the UI extension can be supplied but optional.
 
-        os_log("connecting to room...", log: logger, type: .debug)
-
-        let options = ConnectOptions(
-            // do not subscribe since this is for publish only
-            autoSubscribe: false
-        )
-
-        LiveKit.connect("wss://rtc.unxpected.co.jp",
-                        "",
-                        options: options,
-                        delegate: self).then { room in
-            self.room = room
-            os_log("connected to room", log: logger, type: .debug)
-            do {
-                let track = try LocalVideoTrack.createReplayKitTrack(name: "screen")
-                self.videoTrack = track
-
-                room.localParticipant?.publishVideoTrack(track: track).then({ _ in
-                    os_log("did publish video track", log: logger, type: .debug)
-                })
-            } catch _ {
-                os_log("failed to publish local track", log: logger, type: .error)
-            }
+        ipcClient.connect(SampleHandler.ipcName)
+        os_log("broadcast started", log: logger, type: .debug)
+        if ipcClient.connected {
+            os_log("ipc connected", log: logger, type: .debug)
+        } else {
+            super.finishBroadcastWithError(NSError(domain: "", code: 0, userInfo: nil))
+            os_log("ipc not connected", log: logger, type: .debug)
         }
+        //        let options = ConnectOptions(
+        //            // do not subscribe since this is for publish only
+        //            autoSubscribe: false
+        //        )
+        //
+        //        LiveKit.connect("wss://rtc.unxpected.co.jp",
+        //                        "",
+        //                        options: options,
+        //                        delegate: self).then { room in
+        //            self.room = room
+        //            os_log("connected to room", log: logger, type: .debug)
+        //            do {
+        //                let track = try LocalVideoTrack.createReplayKitTrack(name: "screen")
+        //                self.videoTrack = track
+        //
+        //                room.localParticipant?.publishVideoTrack(track: track).then({ _ in
+        //                    os_log("did publish video track", log: logger, type: .debug)
+        //                })
+        //            } catch _ {
+        //                os_log("failed to publish local track", log: logger, type: .error)
+        //            }
+        //        }
     }
 
     override func broadcastPaused() {
@@ -61,28 +65,44 @@ class SampleHandler: RPBroadcastSampleHandler {
         // User has requested to finish the broadcast.
         os_log("broadcast finished", log: logger, type: .debug)
 
-        guard let room = room else {
-            return
-        }
-
-        room.disconnect()
+        ipcClient.close()
+        //        guard let room = room else {
+        //            return
+        //        }
+        //
+        //        room.disconnect()
     }
 
-    //    override func finishBroadcastWithError(_ error: Error) {
-    //        //
-    //    }
-
     override func processSampleBuffer(_ sampleBuffer: CMSampleBuffer, with sampleBufferType: RPSampleBufferType) {
-
-        // logger.debug("processSampleBuffer \(sampleBuffer.totalSampleSize, format: .byteCount) \(sampleBufferType.rawValue, format:.decimal())")
 
         os_log("process sample buffer", log: logger, type: .debug)
 
         switch sampleBufferType {
         case RPSampleBufferType.video:
-            if let capturer = videoTrack?.capturer as? ReplayKitCapturer {
-                capturer.encodeSampleBuffer(sampleBuffer)
+
+            guard let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+                return
             }
+
+            let timeStamp = CMSampleBufferGetPresentationTimeStamp(sampleBuffer)
+            let timeStampNs = UInt64(CMTimeGetSeconds(timeStamp) * Double(NSEC_PER_SEC))
+            let pixelFormat = CVPixelBufferGetPixelFormatType(imageBuffer)
+
+            let message = IPCMessage.with {
+                $0.buffer = IPCMessage.Buffer.with({
+                    $0.timestampNs = timeStampNs
+                    $0.buffer = Data(pixelBuffer: imageBuffer)
+                    $0.video = .with({
+                        $0.format = pixelFormat
+                        $0.width = UInt32(CVPixelBufferGetWidth(imageBuffer))
+                        $0.height = UInt32(CVPixelBufferGetHeight(imageBuffer))
+                    })
+                })
+            }
+
+            guard let protoData = try? message.serializedData() else { return }
+            ipcClient.send(protoData, messageId: 1)
+
         default: break
         }
     }
--- Shared/ExampleObservableRoom.swift
+++ Shared/ExampleObservableRoom.swift
@@ -54,6 +54,46 @@ final class ExampleObservableRoom: ObservableRoom {
         }
     }
 
+    lazy var ipcServer = IPCServer(onReceivedData: { _, messageId, data in
+        print("IPC Received data \(messageId)")
+
+        guard let t = self.localVideo?.track as? LocalVideoTrack,
+              let capturer = t.capturer as? ReplayKitCapturer else {
+            print("Capturer not ready")
+            return
+        }
+
+        guard let message = try? IPCMessage(serializedData: data) else {
+            print("Failed to decode message")
+            return
+        }
+        
+        if case let .buffer(bufferMessage) = message.type,
+           case let .video(videoMessage) = bufferMessage.type {
+            
+            let pixelBuffer = CVPixelBuffer.from(bufferMessage.buffer,
+                                                 width: Int(videoMessage.width),
+                                                 height: Int(videoMessage.height),
+                                                 pixelFormat: videoMessage.format)
+            
+            capturer.capture(pixelBuffer: pixelBuffer,
+                             timeStampNs: bufferMessage.timestampNs)
+        }
+
+    })
+
+    static let ipcName = "group.livekit-example.broadcast.buffer-ipc"
+
+    override init(_ room: Room) {
+        super.init(room)
+
+        ipcServer.listen(ExampleObservableRoom.ipcName)
+        //        public var ipcClient = IPCClient()
+        //
+    }
+
+    //    var screenShareTrack: LocalVideoTrack?
+
     func togglePublishCamera() {
 
         guard let localParticipant = room.localParticipant else {
@@ -62,39 +102,40 @@ final class ExampleObservableRoom: ObservableRoom {
             return
         }
 
-        localParticipant.setCamera(enabled: !localParticipant.isCameraEnabled(),
-                                   interceptor: interceptor).then { publication in
-                                    self.localVideo = publication
-                                   }
+        //        localParticipant.setCamera(enabled: !localParticipant.isCameraEnabled(),
+        //                                   interceptor: interceptor).then { publication in
+        //                                    self.localVideo = publication
+        //                                   }
 
         //
         // The following code is an example how to publish without using the simplified apis
         //
-        //        if let localVideo = self.localVideo {
-        //            // Try to un-publish the camera
-        //            localParticipant.unpublish(publication: localVideo).then {
-        //                // Update UI
-        //                self.localVideo = nil
-        //            }.catch { error in
-        //                // Failed to un-publish
-        //                print(error)
-        //            }
-        //
-        //        } else {
-        //            // Try to get the camera track
-        //            if let track = try? LocalVideoTrack.createCameraTrack(name: "camera",
-        //                                                                  interceptor: interceptor) {
-        //                // We got the camera track, now try to publish
-        //                localParticipant.publishVideoTrack(track: track).then { pub in
-        //                    // Update UI
-        //                    self.localVideo = pub
-        //                }.catch { error in
-        //                    // If failed to publish, stop the track
-        //                    track.stop()
-        //                    print(error)
-        //                }
-        //            }
-        //        }
+        if let localVideo = self.localVideo {
+            // Try to un-publish the camera
+            localParticipant.unpublish(publication: localVideo).then {
+                // Update UI
+                self.localVideo = nil
+            }.catch { error in
+                // Failed to un-publish
+                print(error)
+            }
+
+        } else {
+            // Try to get the camera track
+            //            if let track = try? LocalVideoTrack.createCameraTrack(name: "camera",
+            //                                                                  interceptor: interceptor) {
+            let track = LocalVideoTrack.createReplayKitTrack(name: "screen")
+            // We got the camera track, now try to publish
+            localParticipant.publishVideoTrack(track: track).then { pub in
+                // Update UI
+                self.localVideo = pub
+            }.catch { error in
+                // If failed to publish, stop the track
+                track.stop()
+                print(error)
+            }
+        }
+
     }
 
     func togglePublishMicrophone() {
