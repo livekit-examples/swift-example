3df1ac4278ac5afb867d945fe855e06d32a8951c
davidliu <davidliu@deviange.net>
2022-06-10 21:51:12 +0900

    Convert to using the SDK SampleHandler

--- iOS/BroadcastExt/SampleHandler.swift
+++ iOS/BroadcastExt/SampleHandler.swift
@@ -1,83 +1,15 @@
-import ReplayKit
+//
+//  SampleHandler.swift
+//  LiveKitExample
+//
+//  Created by David Liu on 6/9/22.
+//
+
+import Foundation
 import LiveKit
-import WebRTC
-import Promises
-import os.log
-
-let logger = OSLog(subsystem: "io.livekit.example.screen-broadcaster",
-                   category: "Broadcaster")
-
-class SampleHandler: RPBroadcastSampleHandler {
-
-    lazy var room = Room()
-    var bufferTrack: LocalVideoTrack?
-    var publication: LocalTrackPublication?
-
-    override func broadcastStarted(withSetupInfo setupInfo: [String: NSObject]?) {
-
-        os_log("broadcast started", log: logger, type: .debug)
-
-        if let ud = UserDefaults(suiteName: "group.io.livekit.example.SwiftSDK.1"),
-           let url = ud.string(forKey: "url"),
-           let token = ud.string(forKey: "token") {
-
-            let connectOptions = ConnectOptions(
-                // do not subscribe since this is for publish only
-                autoSubscribe: false,
-                publishOnlyMode: "screen_share"
-            )
-
-            let roomOptions = RoomOptions(
-                defaultVideoPublishOptions: VideoPublishOptions(
-                    simulcast: false
-                )
-            )
-
-            room.connect(url,
-                         token,
-                         connectOptions: connectOptions,
-                         roomOptions: roomOptions).then { (room) -> Promise<LocalTrackPublication> in
-                            self.bufferTrack = LocalVideoTrack.createBufferTrack()
-                            return room.localParticipant!.publishVideoTrack(track: self.bufferTrack!)
-                         }.then { publication in
-                            self.publication = publication
-                         }
-        } else {
-            os_log("broadcast connect failed", log: logger, type: .debug)
-        }
-    }
-
-    override func broadcastPaused() {
-        // User has requested to pause the broadcast. Samples will stop being delivered.
-        os_log("broadcast paused", log: logger, type: .debug)
-    }
-
-    override func broadcastResumed() {
-        // User has requested to resume the broadcast. Samples delivery will resume.
-        os_log("broadcast resumed", log: logger, type: .debug)
-    }
-
-    override func broadcastFinished() {
-        // User has requested to finish the broadcast.
-        os_log("broadcast finished", log: logger, type: .debug)
-
-        room.disconnect()
-    }
-
-    override func processSampleBuffer(_ sampleBuffer: CMSampleBuffer, with sampleBufferType: RPSampleBufferType) {
-
-        // os_log("processSampleBuffer", log: logger, type: .debug)
-
-        switch sampleBufferType {
-        case RPSampleBufferType.video:
-
-            guard let capturer = bufferTrack?.capturer as? BufferCapturer else {
-                return
-            }
-
-            capturer.capture(sampleBuffer)
-
-        default: break
-        }
+class SampleHandler : LKSampleHandler {
+    
+    override func appGroupIdentifier() -> String {
+        return "group.io.livekit.example.SwiftSDK.1"
     }
 }
