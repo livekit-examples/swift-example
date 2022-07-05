3bfb5d952eff8c8c509bdf8479f25f6a326d94a9
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2022-07-05 08:13:35 +0700

    mix

--- iOS/BroadcastExt/SampleHandler.swift
+++ iOS/BroadcastExt/SampleHandler.swift
@@ -9,40 +9,74 @@ let logger = OSLog(subsystem: "io.livekit.example.screen-broadcaster",
 
 class SampleHandler: RPBroadcastSampleHandler {
 
-    lazy var room = Room()
+    let room = Room()
     var bufferTrack: LocalVideoTrack?
-    var publication: LocalTrackPublication?
+    var audioTrack: LocalAudioTrack?
+
+    var videoPublication: LocalTrackPublication?
+    var audioPublication: LocalTrackPublication?
 
     override func broadcastStarted(withSetupInfo setupInfo: [String: NSObject]?) {
 
-        os_log("broadcast started", log: logger, type: .debug)
+        os_log("[LiveKitBroadcastExt] broadcast started", log: logger, type: .debug)
 
-        if let ud = UserDefaults(suiteName: "group.livekit-example.broadcast"),
-           let url = ud.string(forKey: "url"),
-           let token = ud.string(forKey: "token") {
+        Room.bypassVoiceProcessing = true
 
-            let connectOptions = ConnectOptions(
-                // do not subscribe since this is for publish only
-                autoSubscribe: false,
-                publishOnlyMode: "screen_share"
-            )
+        let shared = RTCAudioSession.sharedInstance()
+        shared.useManualAudio = true
+        shared.isAudioEnabled = false
 
-            let roomOptions = RoomOptions(
-                defaultVideoPublishOptions: VideoPublishOptions(
-                    simulcast: false
-                )
-            )
-
-            room.connect(url,
-                         token,
-                         connectOptions: connectOptions,
-                         roomOptions: roomOptions).then { (room) -> Promise<LocalTrackPublication> in
-                            self.bufferTrack = LocalVideoTrack.createBufferTrack()
-                            return room.localParticipant!.publishVideoTrack(track: self.bufferTrack!)
-                         }.then { publication in
-                            self.publication = publication
-                         }
+        guard let ud = UserDefaults(suiteName: "group.io.livekit.example.SwiftSDK.1"),
+              let url = ud.string(forKey: "url"),
+              let token = ud.string(forKey: "token") else {
+            os_log("[LiveKitBroadcastExt] url & token unknown", log: logger, type: .debug)
+            return
         }
+
+        let connectOptions = ConnectOptions(
+            // do not subscribe since this is for publish only
+            autoSubscribe: false,
+            publishOnlyMode: "screen_share"
+        )
+
+        let roomOptions = RoomOptions(
+            defaultVideoPublishOptions: VideoPublishOptions(
+                // simulcast: false
+                screenShareSimulcastLayers: [
+                    VideoParameters.presetScreenShareH720FPS15,
+                    VideoParameters.presetScreenShareH1080FPS15,
+                    VideoParameters.presetScreenShareH1080FPS30
+                ]
+            )
+        )
+
+        os_log("[LiveKitBroadcastExt] connecting with %@, %@", log: logger, type: .debug, url as NSString, token as NSString)
+
+        room.connect(url,
+                     token,
+                     connectOptions: connectOptions,
+                     roomOptions: roomOptions).then { (room) -> Promise<[LocalTrackPublication]> in
+
+                        os_log("[LiveKitBroadcastExt] connect success", log: logger, type: .debug)
+
+                        self.bufferTrack = LocalVideoTrack.createBufferTrack()
+                        self.audioTrack = LocalAudioTrack.createTrack()
+                        return all([
+                            room.localParticipant!.publishVideoTrack(track: self.bufferTrack!),
+                            room.localParticipant!.publishAudioTrack(track: self.audioTrack!,
+                                                                     publishOptions: AudioPublishOptions(name: "app_audio",
+                                                                                                         bitrate: 320 * 1024,
+                                                                                                         dtx: false))
+                        ])
+                     }.then { publications in
+
+                        self.videoPublication = publications[0]
+                        self.audioPublication = publications[1]
+
+                     }.catch { error in
+                        os_log("[LiveKitBroadcastExt] connect error: %@", log: logger, type: .debug, error.localizedDescription as NSString)
+                     }
+
     }
 
     override func broadcastPaused() {
@@ -70,10 +104,17 @@ class SampleHandler: RPBroadcastSampleHandler {
         case RPSampleBufferType.video:
 
             guard let capturer = bufferTrack?.capturer as? BufferCapturer else {
+                os_log("[LiveKitBroadcastExt] buffer track is nil", log: logger, type: .debug)
                 return
             }
 
             capturer.capture(sampleBuffer)
+            os_log("[LiveKitBroadcastExt] did capture video buffer", log: logger, type: .debug)
+
+        case RPSampleBufferType.audioApp:
+            let adm = Room.audioDeviceModule
+            adm.mix(sampleBuffer)
+            os_log("[LiveKitBroadcastExt] did capture audio buffer", log: logger, type: .debug)
 
         default: break
         }
