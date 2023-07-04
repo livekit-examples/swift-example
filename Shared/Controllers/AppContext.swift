4e19041d33a5f26f07c2dbaa91c788b95bbccd4a
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2023-07-04 15:11:29 +0800

    impl

--- Shared/Controllers/AppContext.swift
+++ Shared/Controllers/AppContext.swift
@@ -41,20 +41,14 @@ final class AppContext: ObservableObject {
     @Published var outputDevice: RTCAudioDevice = RTCAudioDevice.defaultDevice(with: .output) {
         didSet {
             print("didSet outputDevice: \(String(describing: outputDevice))")
-
-            if !Room.audioDeviceModule.setOutputDevice(outputDevice) {
-                print("failed to set value")
-            }
+            Room.audioDeviceModule.outputDevice = outputDevice
         }
     }
 
     @Published var inputDevice: RTCAudioDevice = RTCAudioDevice.defaultDevice(with: .input) {
         didSet {
             print("didSet inputDevice: \(String(describing: inputDevice))")
-
-            if !Room.audioDeviceModule.setInputDevice(inputDevice) {
-                print("failed to set value")
-            }
+            Room.audioDeviceModule.inputDevice = inputDevice
         }
     }
 
@@ -76,18 +70,8 @@ final class AppContext: ObservableObject {
             print("devices did update")
             // force UI update for outputDevice / inputDevice
             DispatchQueue.main.async {
-
-                // set to default device if selected device is removed
-                if !Room.audioDeviceModule.outputDevices.contains(where: { self.outputDevice == $0 }) {
-                    self.outputDevice = RTCAudioDevice.defaultDevice(with: .output)
-                }
-
-                // set to default device if selected device is removed
-                if !Room.audioDeviceModule.inputDevices.contains(where: { self.inputDevice == $0 }) {
-                    self.inputDevice = RTCAudioDevice.defaultDevice(with: .input)
-                }
-
-                self.objectWillChange.send()
+                self.outputDevice = Room.audioDeviceModule.outputDevice
+                self.inputDevice = Room.audioDeviceModule.inputDevice
             }
         }
     }
