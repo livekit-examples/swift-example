4065900a3fc797534324a3a78f399c126a07fa1e
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2023-08-15 15:08:56 +0800

    update

--- Shared/AudioProcessors/SimpleReverbAudioCapturePostProcessor.swift
+++ Shared/AudioProcessors/SimpleReverbAudioCapturePostProcessor.swift
@@ -4,22 +4,18 @@ import WebRTC
 
 class SimpleReverbAudioCapturePostProcessor: NSObject, RTCAudioCustomProcessingDelegate {
 
-    private var delayBuffer: [Float]
+    private var delayBuffer: [Float] = []
     private let delayBufferSize: Int = 4410  // 0.1 second at 44.1kHz
     private var delayBufferIndex: Int = 0
     private let feedback: Float = 0.6
 
-    override init() {
-        delayBuffer = Array(repeating: 0.0, count: delayBufferSize)
-        super.init()
-    }
-
     func initialize(sampleRateHz: Int, numChannels: Int) {
         print("\(String(describing: self)) initialize(sampleRateHz: \(sampleRateHz), numChannels: \(numChannels))")
+        delayBuffer = Array(repeating: 0.0, count: delayBufferSize)
     }
 
     func process(audioBuffer: RTCAudioBuffer) {
-        print("\(String(describing: self)) process(audioBuffer: \(audioBuffer))")
+        // print("\(String(describing: self)) process(audioBuffer: \(audioBuffer))")
 
         for channelIndex in 0..<audioBuffer.channels {
             let channelBuffer = audioBuffer.rawBuffer(forChannel: channelIndex)
