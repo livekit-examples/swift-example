eba27f222dbda15e4649231792dfa3474979d5dd
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2023-08-29 13:18:19 +0900

    fix merge

--- Shared/ParticipantView.swift
+++ Shared/ParticipantView.swift
@@ -109,123 +109,6 @@ struct ParticipantView: View {
                             .lineLimit(1)
                             .truncationMode(.tail)
 
-                        <<<<<<< HEAD
-                            //  if let publication = participant.mainVideoPublication,
-                            //     !publication.muted {
-                            //
-                            //      // is remote
-                            //      if let remotePub = publication as? RemoteTrackPublication {
-                            //          Menu {
-                            //              if case .subscribed = remotePub.subscriptionState {
-                            //                  Button {
-                            //                      remotePub.set(subscribed: false)
-                            //                  } label: {
-                            //                      Text("Unsubscribe")
-                            //                  }
-                            //              } else if case .unsubscribed = remotePub.subscriptionState {
-                            //                  Button {
-                            //                      remotePub.set(subscribed: true)
-                            //                  } label: {
-                            //                      Text("Subscribe")
-                            //                  }
-                            //
-                            //              }
-                            //          } label: {
-                            //              if case .subscribed = remotePub.subscriptionState {
-                            //                  Image(systemSymbol: .videoFill)
-                            //                      .foregroundColor(Color.green)
-                            //              } else if case .notAllowed = remotePub.subscriptionState {
-                            //                  Image(systemSymbol: .exclamationmarkCircle)
-                            //                      .foregroundColor(Color.red)
-                            //              } else {
-                            //                  Image(systemSymbol: .videoSlashFill)
-                            //              }
-                            //          }
-                            //          #if os(macOS)
-                            //          .menuStyle(BorderlessButtonMenuStyle(showsMenuIndicator: true))
-                            //          #elseif os(iOS)
-                            //          .menuStyle(BorderlessButtonMenuStyle())
-                            //          #endif
-                            //          .fixedSize()
-                            //      } else {
-                            //          // local
-                            //          Image(systemSymbol: .videoFill)
-                            //              .foregroundColor(Color.green)
-                            //      }
-                            //
-                            //  } else {
-                            //      Image(systemSymbol: .videoSlashFill)
-                            //          .foregroundColor(Color.white)
-                            //  }
-                            //
-                            //  if let publication = participant.firstAudioPublication,
-                            //     !publication.muted {
-                            //
-                            //      // is remote
-                            //      if let remotePub = publication as? RemoteTrackPublication {
-                            //          Menu {
-                            //              if case .subscribed = remotePub.subscriptionState {
-                            //                  Button {
-                            //                      remotePub.set(subscribed: false)
-                            //                  } label: {
-                            //                      Text("Unsubscribe")
-                            //                  }
-                            //              } else if case .unsubscribed = remotePub.subscriptionState {
-                            //                  Button {
-                            //                      remotePub.set(subscribed: true)
-                            //                  } label: {
-                            //                      Text("Subscribe")
-                            //                  }
-                            //
-                            //              }
-                            //          } label: {
-                            //              if case .subscribed = remotePub.subscriptionState {
-                            //                  Image(systemSymbol: .micFill)
-                            //                      .foregroundColor(Color.orange)
-                            //              } else if case .notAllowed = remotePub.subscriptionState {
-                            //                  Image(systemSymbol: .exclamationmarkCircle)
-                            //                      .foregroundColor(Color.red)
-                            //              } else {
-                            //                  Image(systemSymbol: .micSlashFill)
-                            //              }
-                            //          }
-                            //          #if os(macOS)
-                            //          .menuStyle(BorderlessButtonMenuStyle(showsMenuIndicator: true))
-                            //          #elseif os(iOS)
-                            //          .menuStyle(BorderlessButtonMenuStyle())
-                            //          #endif
-                            //          .fixedSize()
-                            //      } else {
-                            //          // local
-                            //          Image(systemSymbol: .micFill)
-                            //              .foregroundColor(Color.orange)
-                            //      }
-                            //
-                            //  } else {
-                            //      Image(systemSymbol: .micSlashFill)
-                            //          .foregroundColor(Color.white)
-                            //  }
-                            //
-                            //  if participant.connectionQuality == .excellent {
-                            //      Image(systemSymbol: .wifi)
-                            //          .foregroundColor(.green)
-                            //  } else if participant.connectionQuality == .good {
-                            //      Image(systemSymbol: .wifi)
-                            //          .foregroundColor(Color.orange)
-                            //  } else if participant.connectionQuality == .poor {
-                            //      Image(systemSymbol: .wifiExclamationmark)
-                            //          .foregroundColor(Color.red)
-                            //  }
-                            //
-                            //  if participant.encryptionType == .none {
-                            //      Image(systemSymbol: .lockOpenFill)
-                            //          .foregroundColor(.red)
-                            //  } else {
-                            //      Image(systemSymbol: .lockFill)
-                            //          .foregroundColor(.green)
-                            //  }
-                            //
-                            =======
                         if let publication = participant.mainVideoPublication,
                            !publication.muted {
 
@@ -274,7 +157,7 @@ struct ParticipantView: View {
                                 .foregroundColor(Color.white)
                         }
 
-                        if let publication = participant.participant.firstAudioPublication,
+                        if let publication = participant.firstAudioPublication,
                            !publication.muted {
 
                             // is remote
@@ -333,7 +216,7 @@ struct ParticipantView: View {
                                 .foregroundColor(Color.red)
                         }
 
-                        if participant.encryptionType == .none {
+                        if participant.firstTrackEncryptionType == .none {
                             Image(systemSymbol: .lockOpenFill)
                                 .foregroundColor(.red)
                         } else {
@@ -341,7 +224,6 @@ struct ParticipantView: View {
                                 .foregroundColor(.green)
                         }
 
-                        >>>>>>> main
                     }.padding(5)
                     .frame(minWidth: 0, maxWidth: .infinity)
                     .background(Color.black.opacity(0.5))
