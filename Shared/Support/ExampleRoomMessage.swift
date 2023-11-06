abb32a40a7f5698fac4585081da242e519ccc602
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2023-11-07 02:06:06 +0800

    refactor

---
+++ Shared/Support/ExampleRoomMessage.swift
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2023 LiveKit
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+struct ExampleRoomMessage: Identifiable, Equatable, Hashable, Codable {
+    // Identifiable protocol needs param named id
+    var id: String {
+        messageId
+    }
+
+    // message id
+    let messageId: String
+
+    let senderSid: String
+    let senderIdentity: String?
+    let text: String
+
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        lhs.messageId == rhs.messageId
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(messageId)
+    }
+}
--- Shared/Support/Participant+Helpers.swift
+++ Shared/Support/Participant+Helpers.swift
@@ -14,13 +14,7 @@
  * limitations under the License.
  */
 
-import AVFoundation
 import LiveKit
-import SwiftUI
-
-import CoreImage.CIFilterBuiltins
-import ReplayKit
-import WebRTC
 
 public extension Participant {
     var mainVideoPublication: TrackPublication? {
@@ -33,27 +27,5 @@ public extension Participant {
 
     var subVideoTrack: VideoTrack? {
         firstScreenShareVideoTrack != nil ? firstCameraVideoTrack : nil
-    }
-}
-
-struct ExampleRoomMessage: Identifiable, Equatable, Hashable, Codable {
-    // Identifiable protocol needs param named id
-    var id: String {
-        messageId
-    }
-
-    // message id
-    let messageId: String
-
-    let senderSid: String
-    let senderIdentity: String?
-    let text: String
-
-    static func == (lhs: Self, rhs: Self) -> Bool {
-        lhs.messageId == rhs.messageId
-    }
-
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(messageId)
     }
 }
