0b03be82cc3e757148ae80073fff418e31ba63d0
cloudwebrtc <duanweiwei1982@gmail.com>
2024-01-25 15:12:43 +0800

    update.

--- Shared/Views/ScreenShareSourcePickerView.swift
+++ Shared/Views/ScreenShareSourcePickerView.swift
@@ -1,25 +1,39 @@
-import Foundation
+/*
+ * Copyright 2023 LiveKit
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import CoreGraphics
-import SwiftUI
+import Foundation
 import LiveKit
+import SwiftUI
 
 #if os(macOS)
 
-class ScreenShareSourcePickerCtrl: ObservableObject {
-
-    @Published var tracks = [LocalVideoTrack]()
-    @Published var mode: ScreenShareSourcePickerView.Mode = .display {
-        didSet {
-            guard oldValue != mode else { return }
-            Task {
-                await restartTracks()
+    @available(macOS 12.3, *)
+    class ScreenShareSourcePickerCtrl: ObservableObject {
+        @Published var tracks = [LocalVideoTrack]()
+        @Published var mode: ScreenShareSourcePickerView.Mode = .display {
+            didSet {
+                guard oldValue != mode else { return }
+                Task {
+                    try await restartTracks()
+                }
             }
         }
-    }
 
-    private func restartTracks() async {
-
-        Task {
+        private func restartTracks() async throws {
             // stop in parallel
             await withThrowingTaskGroup(of: Void.self) { group in
                 for track in tracks {
@@ -29,7 +43,7 @@ class ScreenShareSourcePickerCtrl: ObservableObject {
                 }
             }
 
-            let sources = try await MacOSScreenCapturer.sources(for: (mode == .display ? .display : .window))
+            let sources = try await MacOSScreenCapturer.sources(for: mode == .display ? .display : .window)
             let options = ScreenShareCaptureOptions(dimensions: .h360_43, fps: 5)
             let _newTracks = sources.map { LocalVideoTrack.createMacOSScreenShareTrack(source: $0, options: options) }
 
@@ -46,93 +60,91 @@ class ScreenShareSourcePickerCtrl: ObservableObject {
                 }
             }
         }
-    }
 
-    init() {
-        Task {
-            await restartTracks()
-        }
-    }
-
-    deinit {
-
-        print("\(type(of: self)) deinit")
-
-        // copy
-        let _tracks = tracks
-
-        Task {
-            // stop in parallel
-            await withThrowingTaskGroup(of: Void.self) { group in
-                for track in _tracks {
-                    group.addTask {
-                        try await track.stop()
-                    }
-                }
+        init() {
+            Task {
+                try await restartTracks()
             }
         }
-    }
-}
 
-typealias OnPickScreenShareSource = (MacOSScreenCaptureSource) -> Void
+        deinit {
+            print("\(type(of: self)) deinit")
 
-struct ScreenShareSourcePickerView: View {
+            // copy
+            let _tracks = tracks
 
-    public enum Mode {
-        case display
-        case window
-    }
-
-    @ObservedObject var ctrl = ScreenShareSourcePickerCtrl()
-
-    let onPickScreenShareSource: OnPickScreenShareSource?
-
-    private var columns = [
-        GridItem(.fixed(250)),
-        GridItem(.fixed(250))
-    ]
-
-    init(onPickScreenShareSource: OnPickScreenShareSource? = nil) {
-        self.onPickScreenShareSource = onPickScreenShareSource
-    }
-
-    var body: some View {
-
-        VStack {
-            Picker("", selection: $ctrl.mode) {
-                Text("Entire Screen").tag(ScreenShareSourcePickerView.Mode.display)
-                Text("Application Window").tag(ScreenShareSourcePickerView.Mode.window)
-            }
-            .pickerStyle(SegmentedPickerStyle())
-
-            ScrollView(.vertical, showsIndicators: true) {
-                LazyVGrid(columns: columns,
-                          alignment: .center,
-                          spacing: 10) {
-
-                    ForEach(ctrl.tracks) { track in
-                        ZStack {
-                            SwiftUIVideoView(track, layoutMode: .fit)
-                                .aspectRatio(1, contentMode: .fit)
-                                .onTapGesture {
-                                    guard let capturer = track.capturer as? MacOSScreenCapturer,
-                                          let source = capturer.captureSource else { return }
-                                    onPickScreenShareSource?(source)
-                                }
-
-                            if let capturer = track.capturer as? MacOSScreenCapturer,
-                               let source = capturer.captureSource as? MacOSWindow,
-                               let appName = source.owningApplication?.applicationName {
-                                Text(appName)
-                                    .shadow(color: .black, radius: 1)
-                            }
+            Task {
+                // stop in parallel
+                await withThrowingTaskGroup(of: Void.self) { group in
+                    for track in _tracks {
+                        group.addTask {
+                            try await track.stop()
                         }
                     }
                 }
             }
-            .frame(minHeight: 350)
         }
     }
-}
+
+    typealias OnPickScreenShareSource = (MacOSScreenCaptureSource) -> Void
+
+    @available(macOS 12.3, *)
+    struct ScreenShareSourcePickerView: View {
+        public enum Mode {
+            case display
+            case window
+        }
+
+        @ObservedObject var ctrl = ScreenShareSourcePickerCtrl()
+
+        let onPickScreenShareSource: OnPickScreenShareSource?
+
+        private var columns = [
+            GridItem(.fixed(250)),
+            GridItem(.fixed(250)),
+        ]
+
+        init(onPickScreenShareSource: OnPickScreenShareSource? = nil) {
+            self.onPickScreenShareSource = onPickScreenShareSource
+        }
+
+        var body: some View {
+            VStack {
+                Picker("", selection: $ctrl.mode) {
+                    Text("Entire Screen").tag(ScreenShareSourcePickerView.Mode.display)
+                    Text("Application Window").tag(ScreenShareSourcePickerView.Mode.window)
+                }
+                .pickerStyle(SegmentedPickerStyle())
+
+                ScrollView(.vertical, showsIndicators: true) {
+                    LazyVGrid(columns: columns,
+                              alignment: .center,
+                              spacing: 10)
+                    {
+                        ForEach(ctrl.tracks) { track in
+                            ZStack {
+                                SwiftUIVideoView(track, layoutMode: .fit)
+                                    .aspectRatio(1, contentMode: .fit)
+                                    .onTapGesture {
+                                        guard let capturer = track.capturer as? MacOSScreenCapturer,
+                                              let source = capturer.captureSource else { return }
+                                        onPickScreenShareSource?(source)
+                                    }
+
+                                if let capturer = track.capturer as? MacOSScreenCapturer,
+                                   let source = capturer.captureSource as? MacOSWindow,
+                                   let appName = source.owningApplication?.applicationName
+                                {
+                                    Text(appName)
+                                        .shadow(color: .black, radius: 1)
+                                }
+                            }
+                        }
+                    }
+                }
+                .frame(minHeight: 350)
+            }
+        }
+    }
 
 #endif
