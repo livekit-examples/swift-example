1dd6eaf6a4e29394da280b379972129404992705
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2024-07-22 16:25:50 +0800

    Implement

---
+++ Multiplatform/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftSDK.1
+//
+//  Created by Hiroshi Horie on 2024/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
