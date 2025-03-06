2699333581f5b15e45ff02efc3b180493c8342a4
Jacob Gelman <3182119+ladvoc@users.noreply.github.com>
2025-03-05 16:00:04 -0800

    Move extension

--- Multiplatform/Views/RoomView.swift
+++ Multiplatform/Views/RoomView.swift
@@ -26,17 +26,6 @@ let adaptiveMin = 300.0
 let toolbarPlacement: ToolbarItemPlacement = .primaryAction
 #endif
 
-extension CIImage {
-    // helper to create a `CIImage` for both platforms
-    convenience init(named name: String) {
-        #if !os(macOS)
-        self.init(cgImage: UIImage(named: name)!.cgImage!)
-        #else
-        self.init(data: NSImage(named: name)!.tiffRepresentation!)!
-        #endif
-    }
-}
-
 #if os(macOS)
 // keeps weak reference to NSWindow
 class WindowAccess: ObservableObject {
